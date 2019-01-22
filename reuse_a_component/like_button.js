ff3b89f260837c4668f987aae2a2adb58173bd69
Max Proske <max@mproske.com>
2019-01-21 20:33:35 -0800

    Finish lesson on reusable components, minify js

---
+++ reuse_a_component/like_button.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const e = React.createElement;
+
+// Define a React component called LikeButton
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+// Find the <div> we added to our HTML in the first step, 
+// and then display our “Like” button React component inside of it
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    // Read the comment ID from a data-* attribute.
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    ReactDOM.render(
+      e(LikeButton, { commentID: commentID }),
+      domContainer
+  );
+});
